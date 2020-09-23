60cdee041d3c6dd812e465205e6603566ec50840
Nguyễn Minh Quân <025804051fb@gmail.com>
2020-09-23 19:03:04 +0700

    Create Fluent API Configuration

---
+++ eShopSolution.Data/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<eShopSolution.Data.Entities.Transaction>
+    {
+        public void Configure(EntityTypeBuilder<eShopSolution.Data.Entities.Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+        }
+    }
+}
